7142063c5105ab60febd647aca53cbad500dca7a
abdullah-tanriverdi <tanriverdi.firatuni@gmail.com>
2024-10-14 09:45:01 +0300

    Code Edited

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,18 +32,19 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.service_vpavgug,
-        import.meta.env.template_mhpiv5l,
-        {
-          from_name: form.name,
-          to_name: "Abdullah TANRIVERDİ",
-          from_email: form.email,
-          to_email: "tanriverdi.firatuni@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.egnziB9Wzt2kKRYkp
-      )
+    emailjs
+    .send(
+      import.meta.env.service_vpavgug,
+      import.meta.env.template_mhpiv5l,
+      {
+        from_name: form.name,
+        to_name: "Abdullah TANRIVERDİ",
+        from_email: form.email,
+        to_email: "tanriverdi.firatuni@gmail.com",
+        message: form.message,
+      },
+      import.meta.env.egnziB9Wzt2kKRYkp
+    )
       .then(
         () => {
           setLoading(false);
